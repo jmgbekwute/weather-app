8c919accfc923ed352cbeda1e3e4be2b2dafadb2
jmgbekwute <120153399+jmgbekwute@users.noreply.github.com>
2023-07-20 10:15:26 +1000

    Inserted HTML forecast into JS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   return `${day} ${hours} ${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+    <div class="weather-forecast-date">
+     ${day}
+     </div>
+    <img src="" alt="" width="42">
+     <div class="weather-forecast-temperature">
+     <span class="weather-forecast-temperature-max">
+     18° 
+     </span>
+     <span class="weather-forecast-temperature-min">
+      12°
+     </span>
+     </div>
+    </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
